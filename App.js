9f859ca76940ea34817a9ab0797ceb8ca6acafb6
Jas Singh Ajmani <164511768+jas001469@users.noreply.github.com>
2024-07-31 18:48:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import QRCodeGenerator from './components/qr-code-generator';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Accordian/>*/}
+      {/*<RandomColor/>*/}
+      {/*<StarRating/>*/}
+      {/*<ImageSlider url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}/>*/}
+        <QRCodeGenerator/>
+    </div>
+  );
+}
+
+export default App;
